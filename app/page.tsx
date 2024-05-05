d2be275708935d87ab710eb4deaa0f80471494d3
raihan <raihanhn20@gmail.com>
2024-05-05 22:55:01 +0600

    adding i18next

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div className="">Hello world</div>;
+  return (
+    <div className="text-center ">
+      <div className="">welcome to i18 internationalisation</div>
+      <Link href={"/about-us"}>About us</Link>
+    </div>
+  );
 }
